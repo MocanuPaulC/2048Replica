4f27e4cacb77d3e92dee0f1f702bd521bfdc52e4
FaisalIbadina <faisal.ibadina@student.kdg.be>
2021-11-20 15:33:29 +0100

    Added screen class

---
+++ src/screens.java
@@ -0,0 +1,21 @@
+public class screens {
+    public static void display_title() {
+        System.out.println
+                ("+-----------------------------+\n" +
+                        "|                             |\n" +
+                        "|            2048             |\n" +
+                        "|          THE GAME           |\n" +
+                        "|                             |\n" +
+                        "|          GROUP 13           |\n" +
+                        "|                             |\n" +
+                        "|                             |\n" +
+                        "|                             |\n" +
+                        "|   START GAME  LEADERBOARD   |\n" +
+                        "|   LOAD GAME                 |\n" +
+                        "|                             |\n" +
+                        "+-----------------------------+");
+
+    }
+
+}
+

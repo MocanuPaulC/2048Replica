70e78f11dccdf5487365f71149f53913457d7760
Mocanu Paul <paul-cristian.mocanu@student.kdg.be>
2021-11-20 15:43:43 +0100

    Tutorial for faisal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import classes.Screens;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        Screens screen = new Screens();
+        screen.display_title();
+        while (true) {
+            System.out.println("Write button desired");
+            String button = scanner.nextLine();
+            switch (button) {
+                case "START GAME":
+                    screen.display_game_board();
+                    break;
+                case "HOME":
+                    screen.display_title();
+                    break;
+                default:
+                    System.out.println("Invalid input");
+                    break;
+            }
+        }
+
     }
 }
---
+++ src/classes/Screens.java
@@ -0,0 +1,29 @@
+package classes;
+
+public class Screens {
+    public Screens() {
+    }
+    public void display_game_board(){
+
+    }
+
+    public void display_title() {
+        System.out.println
+                ("+-----------------------------+\n" +
+                        "|                             |\n" +
+                        "|            2048             |\n" +
+                        "|          THE GAME           |\n" +
+                        "|                             |\n" +
+                        "|          GROUP 13           |\n" +
+                        "|                             |\n" +
+                        "|                             |\n" +
+                        "|                             |\n" +
+                        "|   START GAME  LEADERBOARD   |\n" +
+                        "|   LOAD GAME                 |\n" +
+                        "|                             |\n" +
+                        "+-----------------------------+");
+
+    }
+
+}
+
